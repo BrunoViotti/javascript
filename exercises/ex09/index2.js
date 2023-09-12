e775adc8fbd676f0b89b9a6e60ddc79a13de8bd4
Bruno Viotti <brunomarque2002@gmail.com>
2023-09-12 20:02:10 -0300

    Arrumei o codigo da aula ex09 || fiz a aula 10 (Repetições)

--- exercises/ex09/index2.js
+++ exercises/ex09/index2.js
@@ -5,6 +5,6 @@ if (hora < 12) {
     console.log ('Bom dia!')
 } else if (hora <= 18) {
     console.log ('Boa tarde!')
-} else if (hora > 19) {
+} else if (hora >= 19) {
     console.log ('Boa noite!')
 }
---
+++ exercises/ex10/index.js
@@ -0,0 +1,14 @@
+/*
+var c = 1
+while (c <= 23) {
+    console.log(`Tudo bem? ${c}`)
+    c++
+}
+*/
+
+var c = 1
+
+do {
+    console.log(`Tudo bem? ${c}`)
+    c++
+} while (c <= 23)
