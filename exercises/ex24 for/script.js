d143852e9fbbf70d4e8f9b37d1fa5e423d4573f1
Bruno Viotti <brunomarque2002@gmail.com>
2023-10-17 06:59:24 -0300

    novos estudos

---
+++ exercises/ex24 for/script.js
@@ -0,0 +1,13 @@
+for (var n = 0; n <= 10; n++) {
+    console.log (n)
+}
+
+var arr = [1,2,3,4]
+
+for (var j = 0; j < arr.length; j++) {
+    console.log (arr[j]);
+}
+
+for (var x = 5; x < 100; x *= 2) {
+    console.log (x)
+}
