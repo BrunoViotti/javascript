3bacffd715531f190defa138c08781b45c5bb6a1
Bruno Viotti <brunomarque2002@gmail.com>
2023-10-24 07:29:10 -0300

    aulas de js atualizado

---
+++ exercises/ex26 funcoes/script.js
@@ -0,0 +1,32 @@
+function primeirafuncao () {
+    console.log ("Hello world das funções")
+}
+
+primeirafuncao ();
+
+function DizerNome(nome) {
+    console.log ("O nome é: " + nome)
+}
+
+DizerNome ("Matheusw")
+DizerNome ("Bruno")
+DizerNome ("Roger")
+
+var nomeDoBancodeDados = "Jorge"
+
+DizerNome(nomeDoBancodeDados)
+
+function soma(a, b) {
+    var soma = a + b
+    return soma;
+}
+
+var somaUm = soma (3, 3)
+
+console.log(somaUm)
+
+var somadois = soma (4, 4)
+
+console.log(somadois)
+
+console.log(soma (5, 5))
